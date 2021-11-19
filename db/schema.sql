1138355af57b57df4420f9deb7585d1f039615a0
Kelly <kellytso04@gmail.com>
2021-11-19 11:45:45 -0500

    Creates necessary tables in current PostgreSQL database

--- db/schema.sql
+++ db/schema.sql
@@ -16,10 +16,23 @@ CREATE TABLE styles (
   id BIGSERIAL,
   product_id INTEGER,
   name VARCHAR(500),
-  sale_price INTEGER
+  sale_price INTEGER,
+  original_price INTEGER,
+  default_style INTEGER
 );
 
 ALTER TABLE styles ADD CONSTRAINT styles_pkey PRIMARY KEY (id);
+
+
+DROP TABLE IF EXISTS photos;
+CREATE TABLE photos (
+  id BIGSERIAL,
+  style_id INTEGER,
+  url VARCHAR(500),
+  thumbnail_url VARCHAR(500)
+);
+
+ALTER TABLE photos ADD CONSTRAINT photos_pkey PRIMARY KEY (id);
 
 
 DROP TABLE IF EXISTS skus;
@@ -36,6 +49,7 @@ ALTER TABLE skus ADD CONSTRAINT skus_pkey PRIMARY KEY (id);
 DROP TABLE IF EXISTS features;
 CREATE TABLE features (
   id BIGSERIAL,
+  product_id INTEGER,
   type VARCHAR(50),
   value VARCHAR(50)
 );
@@ -54,6 +68,16 @@ CREATE TABLE cart (
 );
 
 ALTER TABLE cart ADD CONSTRAINT cart_pkey PRIMARY KEY (id);
+
+DROP TABLE IF EXISTS related;
+CREATE TABLE related (
+  id BIGSERIAL,
+  current_product INTEGER,
+  related_product INTEGER
+);
+
+ALTER TABLE related ADD CONSTRAINT related_pkey PRIMARY KEY (id);
+
 
 ALTER TABLE styles ADD CONSTRAINT styles_product_id_fkey FOREIGN KEY (product_id) REFERENCES products(id);
 ALTER TABLE skus ADD CONSTRAINT skus_style_id_fkey FOREIGN KEY (style_id) REFERENCES styles(id);
