b6820fd9857b50ca7ff88bc8a3d7f4a0001345fc
Kelly <kellytso04@gmail.com>
2021-11-29 18:07:40 -0500

    Creates App component

---
+++ client/index.jsx
@@ -0,0 +1,12 @@
+import react from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div className='app'>
+      I am your app :D
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
