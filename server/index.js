c24e3e558fe8fa79074fdecc402d8df3ccefb2d5
Kelly <kellytso04@gmail.com>
2021-11-29 10:23:49 -0500

    Created proxy server

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const axios = require('axios');
+// TODO: Import server helpers
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+// TODO: Create routes
+
