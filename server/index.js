ca668315ea4f2be350ae476bcfecc518a524d5e2
Kelly <kellytso04@gmail.com>
2021-11-29 10:52:57 -0500

    Listed out all routes to be built

--- server/index.js
+++ server/index.js
@@ -4,8 +4,45 @@ const axios = require('axios');
 
 const app = express();
 
+// -- Middleware -------------------------------------------------------------------------
 app.use(express.json());
 app.use(express.urlencoded());
 
-// TODO: Create routes
 
+// -- Routes -----------------------------------------------------------------------------
+// TODO: Build out all your routes!
+app.get('/products', (req, res) => {
+
+});
+
+app.get('/products/:product_id', (req, res) => {
+
+});
+
+app.get('/features/:product_id', (req, res) => {
+
+});
+
+app.get('/styles/:product_id', (req, res) => {
+
+});
+
+app.get('/skus/:product_id', (req, res) => {
+
+});
+
+app.get('/photos/:style_id', (req, res) => {
+
+})
+
+app.get('/related/:product_id', (req, res) => {
+
+});
+
+app.get('/cart', (req, res) => {
+
+});
+
+app.post('/cart', (req, res) => {
+
+});
