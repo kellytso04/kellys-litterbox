e1088756af82a4ac820c8c042a263a52fd845ba7
Kelly <kellytso04@gmail.com>
2021-11-30 12:16:20 -0500

    Modifies tables and adds COPY queries for ETL process

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,9 @@
-DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS products CASCADE;
 CREATE TABLE products (
   id BIGSERIAL,
   name VARCHAR(50),
   slogan VARCHAR(200),
-  description VARCHAR(500),
+  description VARCHAR(1000),
   category VARCHAR(50),
   default_price INTEGER
 );
@@ -11,11 +11,11 @@ CREATE TABLE products (
 ALTER TABLE products ADD CONSTRAINT products_pkey PRIMARY KEY (id);
 
 
-DROP TABLE IF EXISTS styles;
+DROP TABLE IF EXISTS styles CASCADE;
 CREATE TABLE styles (
   id BIGSERIAL,
   product_id INTEGER,
-  name VARCHAR(500),
+  name VARCHAR(1000),
   sale_price INTEGER,
   original_price INTEGER,
   default_style INTEGER
@@ -24,29 +24,29 @@ CREATE TABLE styles (
 ALTER TABLE styles ADD CONSTRAINT styles_pkey PRIMARY KEY (id);
 
 
-DROP TABLE IF EXISTS photos;
+DROP TABLE IF EXISTS photos CASCADE;
 CREATE TABLE photos (
   id BIGSERIAL,
   style_id INTEGER,
-  url VARCHAR(500),
-  thumbnail_url VARCHAR(500)
+  url VARCHAR(1000),
+  thumbnail_url VARCHAR(1000)
 );
 
 ALTER TABLE photos ADD CONSTRAINT photos_pkey PRIMARY KEY (id);
 
 
-DROP TABLE IF EXISTS skus;
+DROP TABLE IF EXISTS skus CASCADE;
 CREATE TABLE skus (
   id BIGSERIAL,
   style_id INTEGER,
-  size VARCHAR(5),
+  size VARCHAR(20),
   quantity INTEGER
 );
 
 ALTER TABLE skus ADD CONSTRAINT skus_pkey PRIMARY KEY (id);
 
 
-DROP TABLE IF EXISTS features;
+DROP TABLE IF EXISTS features CASCADE;
 CREATE TABLE features (
   id BIGSERIAL,
   product_id INTEGER,
@@ -57,7 +57,7 @@ CREATE TABLE features (
 ALTER TABLE features ADD CONSTRAINT features_pkey PRIMARY KEY (id);
 
 
-DROP TABLE IF EXISTS cart;
+DROP TABLE IF EXISTS cart CASCADE;
 CREATE TABLE cart (
   id BIGSERIAL,
   name VARCHAR(200),
@@ -69,7 +69,7 @@ CREATE TABLE cart (
 
 ALTER TABLE cart ADD CONSTRAINT cart_pkey PRIMARY KEY (id);
 
-DROP TABLE IF EXISTS related;
+DROP TABLE IF EXISTS related CASCADE;
 CREATE TABLE related (
   id BIGSERIAL,
   current_product INTEGER,
@@ -78,9 +78,8 @@ CREATE TABLE related (
 
 ALTER TABLE related ADD CONSTRAINT related_pkey PRIMARY KEY (id);
 
-
-ALTER TABLE styles ADD CONSTRAINT styles_product_id_fkey FOREIGN KEY (product_id) REFERENCES products(id);
-ALTER TABLE skus ADD CONSTRAINT skus_style_id_fkey FOREIGN KEY (style_id) REFERENCES styles(id);
-ALTER TABLE features ADD CONSTRAINT features_id_fkey FOREIGN KEY (id) REFERENCES products(id);
-ALTER TABLE cart ADD CONSTRAINT cart_id_fkey FOREIGN KEY (id) REFERENCES products(id);
-ALTER TABLE cart ADD CONSTRAINT cart_sku_id_fkey FOREIGN KEY (sku_id) REFERENCES skus(id);
+COPY products FROM '/Users/kellytso/Documents/GitHub/litterbox/data/products.csv' WITH (FORMAT csv, header);
+COPY styles FROM '/Users/kellytso/Documents/GitHub/litterbox/data/styles.csv' WITH (FORMAT csv, header);
+COPY photos FROM '/Users/kellytso/Documents/GitHub/litterbox/data/photos.csv' WITH (FORMAT csv, header);
+COPY skus FROM '/Users/kellytso/Documents/GitHub/litterbox/data/skus.csv' WITH (FORMAT csv, header);
+COPY features FROM '/Users/kellytso/Documents/GitHub/litterbox/data/features.csv' WITH (FORMAT csv, header);
